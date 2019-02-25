c234b8ef05df4da73605d27a4b14ed80623415e1
Faith Mustapha <f81@MacBook-Air.local>
2019-02-25 14:01:57 -0700

    chore-01-add-math-component

---
+++ src/components/MathComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MathComp extends Component {
+    render(){
+        return(
+            <h1>Hello World from MathComp</h1>
+        );
+    }
+
+}
+
+export default MathComp;
---
+++ src/components/math.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+    const c = a + b;
+    return c;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+export default { sum, subtract, multiply, divide };
